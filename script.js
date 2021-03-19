b5c6ac9e3a452369b059a5e014ce36f0d5885fb8
Globel99-Dev <77055817+Globel99-Dev@users.noreply.github.com>
2021-03-19 12:56:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -21,9 +21,9 @@ document.querySelector('.check').addEventListener('click', function () {
   if (score > 1) {
     // When there is no input
     if (!guess) {
-      alert("Digite un numero");
+      //alert("Digite un numero");
       displayMessage(`ᓚᘏᗢ No number`);
-      //score -= 1;
+      score -= 1;
       // When player wins.
     } else if (guess === randomNumber) {
       displayMessage(`(●'◡'●) You Win`);

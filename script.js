d765ff8e750cc768ea191b436a88821b827c8013
Globel99-Dev <77055817+Globel99-Dev@users.noreply.github.com>
2021-03-22 22:32:13 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.querySelector('.message').textContent = '🍕 Indovina il numero...';
 document.querySelector('.number').textContent = '10';
 document.querySelector('.score ').textContent = '70';
 document.querySelector('.guess').value = '10';*/
+const overlay = document.querySelector('.overlay');
 let randomNumber = Math.floor(Math.random() * 20) + 1;
 console.log(`primer ${randomNumber}`);
 let score = 20;
@@ -11,9 +12,25 @@ let highScore = 0;
 const displayMessage = function (message) {
   document.querySelector('.message').textContent = message;
 };
+
 const displayScore = function (score) {
   document.querySelector('.score').textContent = score;
 };
+
+const reset = function () {
+  score = 20;
+  randomNumber = Math.floor(Math.random() * 20) + 1;
+  console.log(`Segundo ${randomNumber}`);
+  displayMessage('Iniziamo...');
+  displayScore(score);
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = ``;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.overlay').textContent = 'DIGITA UN NUMERO';
+  document.querySelector('.overlay').style.display = 'none';
+};
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   //console.log(typeof guess);
@@ -21,15 +38,26 @@ document.querySelector('.check').addEventListener('click', function () {
   if (score > 1) {
     // When there is no input
     if (!guess) {
-      //alert("Digite un numero");
-      displayMessage(`ᓚᘏᗢ No number`);
+      document.querySelector('.overlay').style.display = 'block';
+      document.querySelector('body').style.backgroundColor = '#f54242';
+      //displayMessage(`ᓚᘏᗢ Manca numero`);
       score -= 1;
+      displayScore(score);
+      document.querySelector('.overlay').addEventListener('click', function () {
+        document.querySelector('.overlay').style.display = 'none';
+        document.querySelector('body').style.backgroundColor = '#222';
+      });
       // When player wins.
     } else if (guess === randomNumber) {
-      displayMessage(`(●'◡'●) You Win`);
+      document.querySelector('.overlay').style.display = 'block';
+      //displayMessage(`(●'◡'●) Hai vinto`);
       document.querySelector('body').style.backgroundColor = '#0398fc';
       document.querySelector('.number').style.width = '30rem';
       document.querySelector('.number').textContent = randomNumber;
+      document.querySelector('.overlay').textContent = `(●'◡'●) Hai vinto`;
+      document.querySelector('.overlay').addEventListener('click', function () {
+        reset();
+      });
       if (score > highScore) {
         highScore = score;
         document.querySelector('.highscore').textContent = highScore;
@@ -37,26 +65,19 @@ document.querySelector('.check').addEventListener('click', function () {
       // When guess is wrong
     } else if (guess !== randomNumber) {
       displayMessage(
-        guess > randomNumber ? `(☞ﾟヮﾟ)☞ Too high` : `☜(ﾟヮﾟ☜) Too low`
+        guess > randomNumber ? `(☞ﾟヮﾟ)☞ Troppo alto` : `☜(ﾟヮﾟ☜) Troppo basso`
       );
       score -= 1;
       displayScore(score);
     }
     // When score < 1, then you lose.
   } else {
-    displayMessage(`(╯°□°）╯︵ ┻━┻ You lose`);
+    displayMessage(`(╯°□°）╯︵ ┻━┻ Hai perso`);
     document.querySelector('.score').textContent = 0;
   }
 });
 //Resetting all values
 document.querySelector('.again').addEventListener('click', function () {
-  score = 20;
-  randomNumber = Math.floor(Math.random() * 20) + 1;
-  console.log(`Segundo ${randomNumber}`);
-  displayMessage('Start guessing...');
-  displayScore(score);
-  document.querySelector('.number').textContent = '?';
-  document.querySelector('.guess').value = ``;
-  document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.number').style.width = '15rem';
+  reset();
 });
+// ADD MODAL WITH MESSAGE WHEN WINS AND WHEN NO NUMBER AND MAYBE WHEN LOSE
